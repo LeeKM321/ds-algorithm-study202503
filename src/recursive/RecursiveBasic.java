6324d1af6ab8c2479b19b3e5e82411831e6682a6
LeeKM321 <stephen4951@gmail.com>
2025-03-18 18:50:09 +0900

    Day04. 재귀함수 학습 (피보나치 수열)

--- src/recursive/RecursiveBasic.java
+++ src/recursive/RecursiveBasic.java
@@ -8,8 +8,17 @@ public class RecursiveBasic {
         System.out.print(n + " ");
     }
 
+    public static int factorial(int n) {
+        if (n == 1) return 1;
+        int result = n * factorial(n - 1);
+        return result;
+    }
+
     public static void main(String[] args) {
-        recursive(3);
+//        recursive(3);
+        int factorial = factorial(5);
+        System.out.println("factorial = " + factorial);
+
     }
 
 }
